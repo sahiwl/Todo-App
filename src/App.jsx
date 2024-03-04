f2cd8d0efd7fbe01e3388fd69affb0218d540fdb
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-03-05 00:26:27 +0530

    feat: Buttons and basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
 
   return (
     <>
-      
+      <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Todo List</h1>
+      <form>
+        <input type="text" className="text-xl border-zinc-800 border-2 m-8 px-4 py-2" id="" placeholder='Enter task' />
+        <input type="text" className="text-xl border-zinc-800 border-2 m-8 px-4 py-2" placeholder='Enter Description' />
+        <button className='bg-black text-white px-4 py-3 font-bold rounded m-5'> Add task</button>
+
+      </form>
     </>
   )
 }
