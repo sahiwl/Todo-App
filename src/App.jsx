8d30b236cabc4f84fb4ef213480bb5b212bf1d4c
Sahil Kumar Ray <piyushkrray1@gmail.com>
2024-03-06 01:41:27 +0530

    UI changes and more

--- src/App.jsx
+++ src/App.jsx
@@ -6,57 +6,83 @@ import './App.css'
 function App() {
   const [title, setTitle] = useState("")
   const [desc, setDesc] = useState("")
-  const [mainTask, setMainTask] = useState("")
+  const [currTask, setCurrTask] = useState("")
+  const me = "</sahil>"
 
-  const submitHandler = (e)=>{
+
+  const submitTasks = (e)=>{
       e.preventDefault()
-      setMainTask([...mainTask, {title, desc}])
-      console.log(mainTask);
+      setCurrTask([...currTask, {title, desc}])
+      console.log(currTask);
   }
 
   
-  const deleteHandler = (i)=>{
-    let copytask = [...mainTask]
-    copytask.splice(i,1)
-    setMainTask(copytask)
+  const deleteTasks = (i)=>{
+    let newTasks = [...currTask]
+    newTasks.splice(i,1)
+    setCurrTask(newTasks)
   }
-  let renderTask = <h2>No Task Available</h2>
+  let taskRender = <h2 className='font-medium text-center'>No Work Today 🤷🏻‍♂️</h2>
+  let s1= "Desc:  "
 
-  if(mainTask.length>0){
-   renderTask= mainTask.map((t,i)=>{ //t -> item, i->index
+  if(currTask.length>0){
+   taskRender= currTask.map((t,i)=>{ //t -> item, i->index
       return(
-        <li key={i} className='flex items-center justify-between'>
-          <div className="flex items-center justify-between mb-5 w-2/3">
-          <h4 className="text-xl font-semibold">{t.title}</h4>
-          <h5 className='text-xl font-medium'>{t.desc}</h5>
-        </div>
-      <button onClick={()=>{
-        deleteHandler(i)
-      }} className='bg-red-200 text-white font-bold'>Delete</button>
+        <li key={i} className='flex items-center justify-between space-x-7'>
+            <div className="flex items-center justify-between mb-2 w-2/3 grid ">
+              <div className="text-xl font-semibold text-wrap whitespace-wrap">
+                  {t.title}
+                  <div className="font-normal">{s1}{t.desc}</div>
+              </div>
+          </div>
+          <button onClick={()=>{
+            deleteTasks(i)
+          }} className='font-bold py-2 px-4 me-2 mb-1 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full  dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'>Delete</button>
         </li> 
         )})
       }
 
   return (
     <>
-      <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Todo List</h1>
+    <div className="flex justify-center items-center">
+      <div className="min-h-1/2 flex justify-center pt-8 rounded-xl grid" style={{width: "60rem"}}>
+      <div className="flex justify-center -mt-7">
+                    <div className="font-medium">made by<a href="https://github.com/sahiwl" className="text-indigo-400 font-semibold" > {me} 🐱‍👤</a>  </div>
+                </div>
+        
+        <div className="rounded-2xl" style={{backgroundColor: "#1B1A55"}}>
+          <h1 className='p-5 text-5xl text-slate-300 font-bold text-center dark:hover:text-indigo-300'> <a href='https://github.com/sahiwl/Todo-App'>Todo List</a></h1> 
+          
+          <form onSubmit={submitTasks} className='grid grid-cols-5'>
+            <input type="text" 
+            className="text-xl text-gray-900 m-8 px-4 py-2 rounded-2xl focus:outline-none col-span-2 placeholder-gray-950" 
+            style={{backgroundColor: '#9290C3'}} 
+            placeholder='I want to....' value={title} 
+            onChange={(e)=>{
+              setTitle(e.target.value)
+              }}/>
 
-      <form onSubmit={submitHandler}>
-        <input type="text" className="text-xl border-zinc-800 border-2 m-8 px-4 py-2" placeholder='Enter task' value={title} onChange={(e)=>{
-          setTitle(e.target.value)
-          }}/>
-        <input type="text" className="text-xl border-zinc-800 border-2 m-8 px-4 py-2" placeholder='Enter Description' value={desc} onChange={(e)=>{
-          setDesc(e.target.value)
-          }}/>
-        <button className='bg-black text-white px-4 py-3 font-bold rounded m-5'> Add task</button>
+            <input type="text" 
+            className="text-xl focus:outline-none rounded-2xl m-8 px-4 py-2 col-span-2 placeholder-gray-950" placeholder='Enter Description' 
+            style={{backgroundColor: '#9290C3'}} 
+            value={desc} 
+            onChange={(e)=>{
+              setDesc(e.target.value)
+              }}/>
 
-      </form>
-      <hr />
-      <div className="p-8 bg-slate-200">
-          <ul>
-            {renderTask}
-          </ul>
+            <button className='font-bold m-7 text-sm font-medium focus:outline-none rounded-2xl dark:bg-violet-500 dark:hover:bg-violet-700'> Add task</button>
+          </form>
+
+          <div className="flex justify-center items-center">
+            <div className="text-pretty p-8 rounded-xl mb-3 min-w-96" style={{background: '#9290C3'}}>
+                    {taskRender}
+            </div>
+          </div>
+          
+        </div>
       </div>
+    </div>
+        
     </>
   )
 }
